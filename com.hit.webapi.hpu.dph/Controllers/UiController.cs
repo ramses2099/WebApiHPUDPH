f3c564011743723cf87beaf2d35ec35040086872
jose.encarnacion <jose.encarnacion@LPT-INF-144>
2017-11-20 14:02:02 -0400

    commit

--- com.hit.webapi.hpu.dph/Controllers/UiController.cs
+++ com.hit.webapi.hpu.dph/Controllers/UiController.cs
@@ -19,13 +19,5 @@ namespace com.hit.webapi.hpu.dph.Controllers
             return View();
         }
 
-
-        public ActionResult TestRNTT()
-        {
-            ViewBag.Title = "UI Windows";
-
-            return View();
-        }
-
     }
 }
